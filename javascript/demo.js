430b12eca744da13d3415da220cd817717026b1f
Mustafa Saifee <saifeemustafaq@gmail.com>
2020-05-26 12:49:41 +0530

    changes

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,5 +1,8 @@
 function okay() {
     $("#errorbox").dialog("close");
+    document.getElementById("mainDiv").style.opacity = 1;
+    document.getElementsByTagName("h1").style.opacity = 1;
+
 
 }
 
@@ -15,7 +18,7 @@ function valid() {
     for (var key in data) {
         if (key === u && data[key] === p) {
             console.log("correct credential");
-            return true;
+            return okay();
         } else {
             console.log("incorrect credentials");
             return false;
@@ -24,3 +27,7 @@ function valid() {
     }
 
 }
+
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// }, false);
